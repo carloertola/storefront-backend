3dbeb768eff83d61b3e91e455c5d754cf3cd636a
carloertola <carloertola@gmail.com>
2023-01-14 01:11:44 +0300

    added conditional for testing

--- src/database.ts
+++ src/database.ts
@@ -7,14 +7,28 @@ const {
     POSTGRES_HOST,
     POSTGRES_DB,
     POSTGRES_USERNAME,
-    POSTGRES_PASSWORD
+    POSTGRES_PASSWORD,
+    POSTGRES_TEST_DB,
+    POSTGRES_TEST_USERNAME,
+    ENV,
 } = process.env;
 
-const sql = new Pool({
-    host: POSTGRES_HOST,
-    database: POSTGRES_DB,
-    user: POSTGRES_USERNAME,
-    password: POSTGRES_PASSWORD,
-});
+let sql;
+
+if(ENV === "test") {
+    sql = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_TEST_DB,
+        user: POSTGRES_TEST_USERNAME,
+        password: POSTGRES_PASSWORD,
+    });
+} else if(ENV === "dev") {
+    sql = new Pool({
+        host: POSTGRES_HOST,
+        database: POSTGRES_DB,
+        user: POSTGRES_USERNAME,
+        password: POSTGRES_PASSWORD,
+    });
+}
 
 export default sql;
