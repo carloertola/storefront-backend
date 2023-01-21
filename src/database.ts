2f25fec86068f7cd250fd687b83da3b3a84aa47f
carloertola <carloertola@gmail.com>
2023-01-21 23:15:58 +0300

    removed a few lines of code and replaced them with more intuitive approaches

--- src/database.ts
+++ src/database.ts
@@ -3,32 +3,22 @@ import { Pool } from 'pg';
 
 dotenv.config();
 
-const {
-    POSTGRES_HOST,
-    POSTGRES_DB,
-    POSTGRES_USERNAME,
-    POSTGRES_PASSWORD,
-    POSTGRES_TEST_DB,
-    POSTGRES_TEST_USERNAME,
-    ENV,
-} = process.env;
-
 let sql;
 
-if(ENV === "test") {
-    sql = new Pool({
-        host: POSTGRES_HOST,
-        database: POSTGRES_TEST_DB,
-        user: POSTGRES_TEST_USERNAME,
-        password: POSTGRES_PASSWORD,
-    });
-} else if(ENV === "dev") {
-    sql = new Pool({
-        host: POSTGRES_HOST,
-        database: POSTGRES_DB,
-        user: POSTGRES_USERNAME,
-        password: POSTGRES_PASSWORD,
-    });
+if (process.env.ENV === 'test') {
+  sql = new Pool({
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_TEST_DB,
+    user: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD
+  });
+} else if (process.env.ENV === 'dev') {
+  sql = new Pool({
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD
+  });
 }
 
 export default sql;
